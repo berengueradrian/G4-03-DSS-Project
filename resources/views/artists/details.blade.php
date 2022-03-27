5896e01dbea7be700a1f7e8c2ccba559f51cf55d
mada <memm25@gcloud.ua.es>
2022-03-27 22:25:15 +0200

    Artist view almost done

--- resources/views/artists/details.blade.php
+++ resources/views/artists/details.blade.php
@@ -1,13 +1,8 @@
 @extends('layouts')
 
-@section('title', 'User details')
-
 @section('content')
-
-<p> {{ $user->name }}</p>
-
-<h1>{{ $user->name }}</h1>
-<p><strong>email:</strong> {{ $user->email }}</p>
-<p>{{ $user->email }}</p>
-<p><a href="{{ action([UserController::class, 'get']) }}">Go back</a></p>
+<h1> User information </h1>
+<p><strong>name:</strong> {{ $artist->name }}</p>
+<p><strong>description:</strong> {{ $artist->description }}</p>
+<p><a href="/api/artists">Go back</a></p>
 @endsection
