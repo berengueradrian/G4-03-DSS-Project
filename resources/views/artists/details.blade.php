1bb951b05b2210a7187299ddac7068d73d50daac
Mario Alcaraz Fuentes <marioalcarazfuentes@gmail.com>
2022-04-04 14:33:18 +0200

    artists detail fix

--- resources/views/artists/details.blade.php
+++ resources/views/artists/details.blade.php
@@ -4,8 +4,8 @@
 <h1> Artist information </h1>
 <p><strong>Name:</strong> {{ $artist->name }}</p>
 <p><strong>Balance:</strong> {{ $artist->balance }}</p>
-<p><strong>Balance:</strong> {{ $artist->volume_sold }}</p>
-<p><strong>Balance:</strong> {{ $artist->description }}</p>
+<p><strong>Volume Sold:</strong> {{ $artist->volume_sold }}</p>
+<p><strong>Description:</strong> {{ $artist->description }}</p>
 
 <p><a href="/api/artists">Go back</a></p>
 @endsection
