fba9e06021a7607b2b602089ed8987d2d6c3e12e
Mario Alcaraz Fuentes <marioalcarazfuentes@gmail.com>
2022-04-07 11:13:38 +0200

    Fix Type errors

--- app/Http/Controllers/TypeController.php
+++ app/Http/Controllers/TypeController.php
@@ -5,18 +5,22 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Type;
 
-class TypeController extends Controller{
+class TypeController extends Controller
+{
 
-    public function get(Type $type){
+    public function get(Type $type)
+    {
         return view('types.details')->with('type', $type);
     }
 
-    public function getAll(){
+    public function getAll()
+    {
         $types = Type::paginate(2);
         return view('types.list')->with('types', $types);
     }
 
-    public function create(Request $data){
+    public function create(Request $data)
+    {
         $type = Type::create([
             'name' => $data->name,
             'description' => $data->email,
@@ -25,26 +29,29 @@ class TypeController extends Controller{
         return response()->json(['success' => true, 'type' => $type]);
     }
 
-    public function delete(Type $type){
-        if(Type::whereId($type->id)->count()){
+    public function delete(Type $type)
+    {
+        if (Type::whereId($type->id)->count()) {
             $type->delete();
             return response()->json(['success' => true, 'type' => $type]);
         }
         return response()->json(['success' => false]);
     }
 
-    public function update(Request $request, Type $type){
-        
+    public function update(Request $request, Type $type)
+    {
+
         $newType = Type::find($type->id);
         $newType->name = $request->name;
-        if($request->filled('description')) {
+        if ($request->filled('description')) {
             $newType->description = $request->description;
         }
-        
+
         $newType->save();
     }
 
-    public function sortByName(Request $request) {
+    public function sortByExclusivity(Request $request)
+    {
         if ($request->sortByExclusivity == 0) {
             $types = Type::orderBy('name', 'DESC')->paginate(2);
         } elseif ($request->sortByExclusivity == 1) {
